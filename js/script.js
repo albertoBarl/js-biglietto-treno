9c641c664e2f1a8b5cbe5533963725f9178067de
albertoBarl <alberto.barletta00@gmail.com>
2022-11-28 16:13:57 +0100

    added prices rules

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 // 1- Chiedere i chilometri da percorrere
-let kms = prompt("Inserisci i chilometri da percorrere")
+let kms = prompt('Inserisci i chilometri da percorrere');
 
 // 2- Chiedere l'età del passeggero
-let age = prompt("Inserisci la tua età")
+let age = prompt('Inserisci la tua età');
 
-// 3- Calcolare il prezzo del biglietto dato da km * 0.21
+// 3.1- Se il passeggero è minorenne avrà uno sconto del 20%
+if(age < 18){
+    let price = 0.21 * kms * 0.80
+    document.getElementById('ticket-price').innerHTML = price
+}
+
+
+// 3.2- Se il passeggero è over 65 avrà uno sconto del 40%
+else if(age > 65){
+    let price = 0.21 * kms * 0.60
+    document.getElementById('ticket-price').innerHTML = price
+}
+
+// 3.3- Prezzo intero
+else{
+    let price = 0.21 * kms
+    document.getElementById('ticket-price').innerHTML = price
+}
