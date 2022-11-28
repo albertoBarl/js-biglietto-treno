2ac8e0c8e239b0a8382a1a536cb7a15d8babb9b5
albertoBarl <alberto.barletta00@gmail.com>
2022-11-28 16:17:09 +0100

    added .toFixed for decimal numbers

--- js/script.js
+++ js/script.js
@@ -6,19 +6,19 @@ let age = prompt('Inserisci la tua età');
 
 // 3.1- Se il passeggero è minorenne avrà uno sconto del 20%
 if(age < 18){
-    let price = 0.21 * kms * 0.80
-    document.getElementById('ticket-price').innerHTML = price
+    let price = (0.21 * kms * 0.80).toFixed(2);
+    document.getElementById('ticket-price').innerHTML = price;
 }
 
 
 // 3.2- Se il passeggero è over 65 avrà uno sconto del 40%
 else if(age > 65){
-    let price = 0.21 * kms * 0.60
-    document.getElementById('ticket-price').innerHTML = price
+    let price = (0.21 * kms * 0.60).toFixed(2);
+    document.getElementById('ticket-price').innerHTML = price;
 }
 
 // 3.3- Prezzo intero
 else{
-    let price = 0.21 * kms
-    document.getElementById('ticket-price').innerHTML = price
+    let price = (0.21 * kms).toFixed(2);
+    document.getElementById('ticket-price').innerHTML = price;
 }
