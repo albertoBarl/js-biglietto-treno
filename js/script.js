122d0ae9aa6e1ed4693ccc31919587295ae147c9
albertoBarl <alberto.barletta00@gmail.com>
2022-11-28 15:47:48 +0100

    added first and second phase

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1- Chiedere i chilometri da percorrere
+let kms = prompt("Inserisci i chilometri da percorrere")
+
+// 2- Chiedere l'età del passeggero
+let age = prompt("Inserisci la tua età")
+
+// 3- Calcolare il prezzo del biglietto dato da km * 0.21
